6bba187663f172bbdd61f78db9f31e23375ba698
김 수정 <kim@gim-ui-iMac.local>
2022-05-29 12:04:27 +0900

    styled-components_keyframes_prac

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, {keyframes} from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>움직인다아</Box>
     </div>
   );
 }
+const boxAnimation = keyframes`
+  0%{
+    top: 0px;
+  }
+  50%{
+    top:700px;
+    left: 500px;
+  }
+  100%{
+    top: 200px;
+    left:800px;
+  }
+`;
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background: slateblue;
+  border-radius:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  color: #fff;
+  position:absolute;
+  top: 20px;
+  left: 20px;
+  animation: ${boxAnimation} 4s infinite linear alternate;
+`;
+
 
 export default App;
